a1410dfac716945566a6a5f8358642d775b79a77
Hrishikesh Mahajan <hrishikesh.22110292@viit.ac.in>
2022-06-08 14:28:40 +0530

    feat: add barcode dictionary with initial entries

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+Barcode_Dictionary = {
+    8901571010554: ("Crocin", 10.00),
+    300450170149: ("Benadryl", 20.00),
+    8901117227781: ("Cofsils", 30.00),
+    8901082000013: ("Charak Kofol", 50.00),
+    312843131057: ("Bayer Chewable Aspirin", 30.00),
+    300055509191: ("Caltrate Vitamins", 40.00),
+    300450481269: ("Motrin IB, Ibuprofen", 320.00),
+    300450212184: ("Imodium Anti-Diarrheal", 200.00),
+    306969234044: ("Boiron MotionCalm", 340.00),
+    763935118119: ("Excedrin Acetaminophen", 150.00),
+}

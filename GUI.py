90c1e22d1901aa4684538cd8e1fc97d4c265f3db
Hrishikesh Mahajan <hrishikesh.22110292@viit.ac.in>
2022-06-27 20:03:00 +0530

    2022-06-27

--- GUI.py
+++ GUI.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 from tkinter import END, INSERT, Button, Entry, Label, Text, Tk
 
 import Barcode
 import Dictionary
+
+now = datetime.now()
+date_time = now.strftime("%Y/%m/%d  %H:%M:%S")
 
 window = Tk()
 
@@ -18,91 +22,151 @@ def reset():
     doctor_name.delete(0, END)
 
 
+def str_round(string):
+    round_figure = round(float(string), 2)
+    if int(round_figure * 10) == (round_figure * 10):
+        return str(round_figure) + "0"
+    else:
+        return str(round_figure)
+
+
 def scan_item():
     global item_index
     code = Barcode.capture()
-    name = Dictionary.Barcode_Dictionary[code][0]
-    price = Dictionary.Barcode_Dictionary[code][1]
     if code in Dictionary.Barcode_Dictionary.keys():
-        item_number.insert(INSERT, str(int(item_index)) + ".\n")
-        item_name.insert(INSERT, name + "\n")
-        mrp.insert(INSERT, str(round(price + 0.00, 2)) + "\n")
-        quantity.insert(INSERT, "1" + "\n")
-        rate.insert(INSERT, str(round(price / 1.12, 2)) + "\n")
-        total.insert(
-            INSERT,
-            str(
-                float(rate.get(item_index, "end-1c linestart"))
-                * float(quantity.get(item_index, "end-1c linestart"))
-            )
-            + "\n",
+        name = Dictionary.Barcode_Dictionary[code][0]
+        price = Dictionary.Barcode_Dictionary[code][1]
+        for counter in range(0, int(item_index)):
+            line = float(counter) + 1
+            print(item_name.get(line, line + 0.99))
+            if name == item_name.get(line, line + 0.99):
+                old_item(line)
+            else:
+                new_item(name, price)
+    sum_all()
+    align_right()
+
+
+def new_item(name, price):
+    global item_index
+    item_number.insert(INSERT, str(int(item_index)) + ".\n")
+    item_name.insert(INSERT, name + "\n")
+    mrp.insert(INSERT, str(round(price + 0.00, 2)) + "\n")
+    quantity.insert(INSERT, "1" + "\n")
+    rate.insert(INSERT, str_round(price / 1.12) + "\n")
+    total.insert(
+        INSERT,
+        str_round(
+            float(rate.get(item_index, "end-1c linestart"))
+            * float(quantity.get(item_index, "end-1c linestart"))
         )
-        disc_amt.insert(
-            INSERT,
-            str(
-                round(
-                    float(total.get(item_index, "end-1c linestart"))
-                    * float(discount_percent.get(1.0, END))
-                    / 100,
-                    2,
-                )
-            )
-            + "\n",
+        + "\n",
+    )
+    disc_amt.insert(
+        INSERT,
+        str_round(
+            float(total.get(item_index, "end-1c linestart"))
+            * float(discount_percent.get(1.0, END))
+            / 100
         )
-        taxable_amt.insert(
-            INSERT,
-            str(
-                round(
-                    float(total.get(item_index, "end-1c linestart"))
-                    - float(disc_amt.get(item_index, "end-1c linestart")),
-                    2,
-                )
-            )
-            + "\n",
+        + "\n",
+    )
+    taxable_amt.insert(
+        INSERT,
+        str_round(
+            float(total.get(item_index, "end-1c linestart"))
+            - float(disc_amt.get(item_index, "end-1c linestart"))
         )
-        c_gst.insert(
-            INSERT,
-            str(round(float(taxable_amt.get(item_index, "end-1c linestart")) * 0.06, 2))
-            + "\n",
+        + "\n",
+    )
+    c_gst.insert(
+        INSERT,
+        str_round(float(taxable_amt.get(item_index, "end-1c linestart")) * 0.06) + "\n",
+    )
+    s_gst.insert(
+        INSERT,
+        str_round(float(taxable_amt.get(item_index, "end-1c linestart")) * 0.06) + "\n",
+    )
+    total_amt.insert(
+        INSERT,
+        str_round(
+            float(mrp.get(item_index, "end-1c linestart"))
+            * float(quantity.get(item_index, "end-1c linestart"))
+            * (1 - float(discount_percent.get(1.0, END)) / 100)
         )
-        s_gst.insert(
-            INSERT,
-            str(round(float(taxable_amt.get(item_index, "end-1c linestart")) * 0.06, 2))
-            + "\n",
-        )
-        total_amt.insert(
-            INSERT,
-            str(
-                round(
-                    float(mrp.get(item_index, "end-1c linestart"))
-                    * float(quantity.get(item_index, "end-1c linestart"))
-                    * (1 - float(discount_percent.get(1.0, END)) / 100),
-                    2,
-                )
-            )
-            + "\n",
-        )
-        item_index = item_index + 1
-        sum_all()
-        item_number.tag_add("Right", "1.0", "end")
-        mrp.tag_add("Right", "1.0", "end")
-        quantity.tag_add("Right", "1.0", "end")
-        rate.tag_add("Right", "1.0", "end")
-        total.tag_add("Right", "1.0", "end")
-        disc_amt.tag_add("Right", "1.0", "end")
-        taxable_amt.tag_add("Right", "1.0", "end")
-        c_gst.tag_add("Right", "1.0", "end")
-        s_gst.tag_add("Right", "1.0", "end")
-        total_amt.tag_add("Right", "1.0", "end")
-        sum_total.tag_add("Right", "1.0", "end")
-        sum_disc_amt.tag_add("Right", "1.0", "end")
-        sum_taxable_amt.tag_add("Right", "1.0", "end")
-        sum_c_gst.tag_add("Right", "1.0", "end")
-        sum_s_gst.tag_add("Right", "1.0", "end")
-        sum_total_amt.tag_add("Right", "1.0", "end")
+        + "\n",
+    )
+    item_index = item_index + 1
+
+
+def old_item(line):
+    current_quantity = int(quantity.get(line, line + 0.99))
+    quantity.delete(line, line + 0.99)
+    quantity.insert(line, str(current_quantity + 1))
+    total.delete(line, line + 0.99)
+    total.insert(
+        line,
+        str_round(
+            float(rate.get(line, line + 0.99)) * float(quantity.get(line, line + 0.99))
+        ),
+    )
+    disc_amt.delete(line, line + 0.99)
+    disc_amt.insert(
+        line,
+        str_round(
+            float(total.get(line, line + 0.99))
+            * float(discount_percent.get(1.0, END))
+            / 100
+        ),
+    )
+    taxable_amt.delete(line, line + 0.99)
+    taxable_amt.insert(
+        line,
+        str_round(
+            float(total.get(line, line + 0.99)) - float(disc_amt.get(line, line + 0.99))
+        ),
+    )
+    c_gst.delete(line, line + 0.99)
+    c_gst.insert(line, str_round(float(taxable_amt.get(line, line + 0.99)) * 0.06))
+    s_gst.delete(line, line + 0.99)
+    s_gst.insert(line, str_round(float(taxable_amt.get(line, line + 0.99)) * 0.06))
+    total_amt.delete(line, line + 0.99)
+    total_amt.insert(
+        line,
+        str_round(
+            float(mrp.get(line, line + 0.99))
+            * float(quantity.get(line, line + 0.99))
+            * (1 - float(discount_percent.get(1.0, END)) / 100)
+        ),
+    )
+
+
+def align_right():
+    item_number.tag_add("Right", "1.0", "end")
+    mrp.tag_add("Right", "1.0", "end")
+    quantity.tag_add("Right", "1.0", "end")
+    rate.tag_add("Right", "1.0", "end")
+    total.tag_add("Right", "1.0", "end")
+    disc_amt.tag_add("Right", "1.0", "end")
+    taxable_amt.tag_add("Right", "1.0", "end")
+    c_gst.tag_add("Right", "1.0", "end")
+    s_gst.tag_add("Right", "1.0", "end")
+    total_amt.tag_add("Right", "1.0", "end")
+    sum_total.tag_add("Right", "1.0", "end")
+    sum_disc_amt.tag_add("Right", "1.0", "end")
+    sum_taxable_amt.tag_add("Right", "1.0", "end")
+    sum_c_gst.tag_add("Right", "1.0", "end")
+    sum_s_gst.tag_add("Right", "1.0", "end")
+    sum_total_amt.tag_add("Right", "1.0", "end")
 
 
 def sum_all():
+    sum(total, sum_total)
+    sum(disc_amt, sum_disc_amt)
+    sum(taxable_amt, sum_taxable_amt)
+    sum(c_gst, sum_c_gst)
+    sum(s_gst, sum_s_gst)
+    sum(total_amt, sum_total_amt)
     sum(total, sum_total)
     sum(disc_amt, sum_disc_amt)
     sum(taxable_amt, sum_taxable_amt)
@@ -117,7 +181,7 @@ def sum(data, result):
         line = float(counter)
         count = count + float(data.get(line, line + 1))
     result.delete("1.0", "end")
-    result.insert(1.0, str(count))
+    result.insert(1.0, str_round(count))
 
 
 def clear():
@@ -133,7 +197,25 @@ def clear():
     s_gst.delete("1.0", "end")
     total_amt.delete("1.0", "end")
     discount_percent.delete("1.0", "end")
+    item_number.delete("1.0", "end")
+    item_name.delete("1.0", "end")
+    mrp.delete("1.0", "end")
+    quantity.delete("1.0", "end")
+    rate.delete("1.0", "end")
+    total.delete("1.0", "end")
+    disc_amt.delete("1.0", "end")
+    taxable_amt.delete("1.0", "end")
+    c_gst.delete("1.0", "end")
+    s_gst.delete("1.0", "end")
+    total_amt.delete("1.0", "end")
+    discount_percent.delete("1.0", "end")
     discount_percent.insert(1.0, "0.00")
+    sum_total.delete("1.0", "end")
+    sum_disc_amt.delete("1.0", "end")
+    sum_taxable_amt.delete("1.0", "end")
+    sum_c_gst.delete("1.0", "end")
+    sum_s_gst.delete("1.0", "end")
+    sum_total_amt.delete("1.0", "end")
 
 
 def exit_function():
@@ -168,6 +250,7 @@ invoice_number.grid(row=0, column=1, sticky="w")
 
 invoice_date = Entry(window, width=25)
 invoice_date.grid(row=1, column=1, sticky="w")
+invoice_date.insert(0, date_time)
 
 patient_id = Entry(window, width=25)
 patient_id.grid(row=2, column=1, sticky="w")
